510f105daa9e8df5fd8f781f2ccd6c7a3a93dd2d
amrit-44404 <amrit44404@proton.me>
2024-08-06 21:31:15 +0545

    new auto install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+# Installation
+sudo pacman -Sy && sudo pacman -S brightnessctl hyprpaper imv lf mpv neovim ttf-hack ttf-hack-nerd waybar bleachbit fastfetch unzip hyprlock newsboat mupdf
+
+# Remove Bloat
+sudo pacman -Rncsu vim dolphin nano dunst kitty ly
+
+# MKdir
+mkdir -p ~/.local/share ~/.config
+
+# Post Installation
+git clone --depth=1 https://gitlab.com/amrit-44404/hyprdots.git ~/hyprdots
+git clone --depth=1 https://gitlab.com/amrit-44404/wall.git ~/.local/share/wall
+
+# Managing Dotfiles
+cp -r ~/hyprdots/.local/share/* ~/.local/share
+cp -r ~/hyprdots/.config/* ~/.config
+cp ~/hyprdots/.bashrc ~/.bashrc
