77801d63744c7405237b0d3505a1cf4519152b6f
amrit-44404 <amrit44404@proton.me>
2024-08-20 18:26:54 +0545

    removed dmenu with wofi

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Installation
-sudo pacman -Sy && sudo pacman -S brightnessctl hyprpaper imv lf mpv neovim ttf-hack ttf-hack-nerd waybar bleachbit fastfetch unzip hyprlock newsboat mupdf noto-fonts-emoji wtype
+sudo pacman -Sy && sudo pacman -S brightnessctl hyprpaper imv lf mpv neovim ttf-hack ttf-hack-nerd waybar bleachbit fastfetch unzip hyprlock newsboat mupdf noto-fonts-emoji wtype wofi
 
 # Remove Bloat
 sudo pacman -Rncsu vim dolphin nano dunst kitty ly
@@ -11,7 +11,6 @@ mkdir -p ~/.local/share ~/.config ~/.local/bin
 # Post Installation
 git clone --depth=1 https://gitlab.com/amrit-44404/hyprdots.git ~/hyprdots
 git clone --depth=1 https://gitlab.com/amrit-44404/wall.git ~/.local/share/wall
-git clone --depth=1 https://gitlab.com/amrit-44404/dmenu.git ~/.config/dmenu
 git clone --depth=1 https://gitlab.com/amrit-44404/dev.git ~/.local/dev
 
 # Managing Dotfiles
@@ -20,6 +19,3 @@ cp -r ~/hyprdots/.local/bin/* ~/.local/bin
 cp -r ~/hyprdots/.config/* ~/.config
 cp ~/hyprdots/.bashrc ~/.bashrc
 cp ~/hyprdots/.inputrc ~/.inputrc
-
-# DMenu
-sudo make -C ~/.config/dmenu/ clean install
