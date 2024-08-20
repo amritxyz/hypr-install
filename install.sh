064e6f442df87e7652c97f24ba2d466ccb1b7646
amrit-44404 <amrit44404@proton.me>
2024-08-20 20:40:00 +0545

    many changes in script

--- install.sh
+++ install.sh
@@ -1,9 +1,32 @@
-# Installation
-sudo pacman -Sy && sudo pacman -S brightnessctl hyprpaper imv lf mpv neovim ttf-hack ttf-hack-nerd waybar bleachbit fastfetch unzip hyprlock newsboat mupdf noto-fonts-emoji wtype wofi
+# Disable Wifi-Power Saver
+read -n1 -rep 'Would you like to disable wifi powersave? (y,n)' WIFI
+if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
+    LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
+    echo -e "The following has been added to $LOC.\n"
+    echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC
+    echo -e "\n"
+    echo -e "Restarting NetworkManager service...\n"
+    sudo systemctl restart NetworkManager
+    sleep 3
+fi
+
+### Install all of the imp pacakges ####
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
+    sudo pacman -Sy --needed base-devel \
+    sudo pacman -S hyprland brightnessctl hyprpaper foot imv lf \
+    mpv neovim ttf-hack ttf-hack-nerd waybar bleachbit fastfetch \
+    unzip hyprlock newsboat mupdf noto-fonts-emoji wtype wofi \
+    bluez bluez-utils
+
+    # Start the bluetooth service
+    echo -e "Starting the Bluetooth Service...\n"
+    sudo systemctl enable --now bluetooth.service
+    sleep 2
 
 # Remove Bloat
-sudo pacman -Rncsu vim dolphin nano dunst kitty ly
-sudo pacman -Scc && sudo pacman -Sy
+#sudo pacman -Rncsu vim dolphin nano dunst kitty ly
+#sudo pacman -Scc && sudo pacman -Sy
 
 # MKdir
 mkdir -p ~/.local/share ~/.config ~/.local/bin
