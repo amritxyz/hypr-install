baa70172d0ab96a4728db81f4d6ad4475bd58179
amritxyz <amrit44404@proton.me>
2025-02-15 22:25:08 +0545

    screen recorder for wayland

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ if [[ $INST == "Y" || $INST == "y" || -z $INST ]]; then
 		sudo pacman -S tesseract-data-nep hyprland brightnessctl hyprpaper foot imv lf \
 		mpv neovim ttf-hack ttf-hack-nerd waybar bleachbit fastfetch unzip hyprlock \
 		newsboat noto-fonts-emoji wtype wofi htop grim slurp man-db zathura zathura-pdf-poppler \
-		vulkan-intel xdg-desktop-portal-gtk adwaita-icon-theme git-lfs \
+		vulkan-intel xdg-desktop-portal-gtk adwaita-icon-theme git-lfs wf-recorder \
 		gimp wget deluge-gtk fzf curl cmatrix gnu-netcat nodejs zed \
 		rust go jdk23-openjdk tmux wl-clipboard bluez bluez-utils
 fi
